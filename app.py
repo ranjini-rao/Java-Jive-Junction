28105b27a0e52e47827dcd66510457ec69943c5d
pallavitripathi3107 <pallavitripathi3107@gmail.com>
2024-01-06 15:12:32 -0600

    merge

--- app.py
+++ app.py
@@ -9,17 +9,18 @@ from sqlalchemy import create_engine
 from datetime import datetime
 import json
 
+
 app = Flask(__name__)
 # Enable CORS for all routes
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Configure the database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:anish2803@localhost:5432/Coffee_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/Coffee_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Database connection
-alchemyEngine = create_engine('postgresql://postgres:anish2803@localhost:5432/Coffee_db', pool_size=20, max_overflow=0);
+alchemyEngine = create_engine('postgresql://username:password@localhost:5432/Coffee_db', pool_size=20, max_overflow=0);
 
 # reflect an existing database into a new model
 base = automap_base()
@@ -134,6 +135,140 @@ filter(Customers.country == 'United States').all()
                 'product_id': row[14],
                 'unit_price': row[15]
                 
+            })
+
+        return jsonify(result)
+    
+# Create a route to fetch and return the data as JSON
+@app.route('/coffeeCountries', methods=['GET'])
+def get_country():
+    with app.app_context():
+        data = db.session.query(Customers.country).\
+        join(Order, Customers.customer_id == Order.customer_id).\
+        join(Order_Items, Order_Items.order_id == Order.order_id).\
+        join(Products, Order_Items.product_id == Products.product_id).distinct().all()
+
+        result = []
+        for row in data:
+            result.append({
+                'country': row[0]
+            })
+        return jsonify(result)
+
+# Create a route to fetch and return the data as JSON
+@app.route('/coffee', methods=['GET'])
+def get_coffee_country():
+    with app.app_context():
+        data = db.session.query(Customers.country, Products.coffee_type).\
+        join(Order, Customers.customer_id == Order.customer_id).\
+        join(Order_Items, Order_Items.order_id == Order.order_id).\
+        join(Products, Order_Items.product_id == Products.product_id).distinct().all()
+
+        result = []
+        for row in data:
+            result.append({
+                'country': row[0],
+                'coffee_type': row[1]
+            })
+        return jsonify(result)
+
+# Create a route to fetch and return the data as JSON
+@app.route('/coffeeYear', methods=['GET'])
+def get_coffee_year():
+    with app.app_context():
+        data = db.session.query(Customers.country, db.func.extract('year', Order.order_date).label('order_year')).\
+        join(Order, Customers.customer_id == Order.customer_id).\
+        join(Order_Items, Order_Items.order_id == Order.order_id).\
+        join(Products, Order_Items.product_id == Products.product_id).distinct().all()
+
+        result = []
+        for row in data:
+            result.append({
+                'country': row[0],
+                'order_year': row[1]
+            })
+        return jsonify(result)
+
+   # Inside your Flask route or function
+@app.route('/get_order_City_CoffeeType')
+def get_order_City_CoffeeType():
+    with app.app_context():
+        data = db.session.query(Customers.city, Customers.country, Customers.postal_code, Products.coffee_type, db.func.extract('year', Order.order_date).label('order_year'),db.func.sum(Order_Items.quantity*Products.size)). \
+        join(Order, Customers.customer_id == Order.customer_id).\
+        join(Order_Items, Order_Items.order_id == Order.order_id).\
+        join(Products, Order_Items.product_id == Products.product_id).group_by(Customers.city,Customers.country,Customers.postal_code,Products.coffee_type,'order_year').all()
+
+        result = []
+        for row in data:
+            result.append({
+                'city':row[0],
+                'country': row[1],
+                'zipcode': row[2],                
+                'coffee_type': row[3],
+                'order_year': row[4],
+                'quantity': row[5]
+            })
+
+        return jsonify(result)
+
+   # Inside your Flask route or function
+@app.route('/get_order_City_ByYear')
+def get_order_City_ByYear():
+    with app.app_context():
+        data = db.session.query(Customers.city, Customers.country, Customers.postal_code, db.func.extract('year', Order.order_date).label('order_year'), db.func.sum(Order_Items.quantity*Products.size)). \
+        join(Order, Customers.customer_id == Order.customer_id).\
+        join(Order_Items, Order_Items.order_id == Order.order_id).\
+        join(Products, Order_Items.product_id == Products.product_id).group_by(Customers.city, Customers.country,Customers.postal_code,'order_year').all()
+
+        result = []
+        for row in data:
+            result.append({
+                'city':row[0],
+                'country': row[1],
+                'zipcode': row[2],
+                'order_year': row[3],                
+                'quantity': row[4]
+            })
+
+        return jsonify(result)
+
+   # Inside your Flask route or function
+@app.route('/get_order_City_ByCoffeType')
+def get_order_City_ByCoffeType():
+    with app.app_context():
+        data = db.session.query(Customers.city, Customers.country, Customers.postal_code, Products.coffee_type, db.func.sum(Order_Items.quantity*Products.size)). \
+        join(Order, Customers.customer_id == Order.customer_id).\
+        join(Order_Items, Order_Items.order_id == Order.order_id).\
+        join(Products, Order_Items.product_id == Products.product_id).group_by(Customers.city, Customers.country,Customers.postal_code, Products.coffee_type).all()
+
+        result = []
+        for row in data:
+            result.append({
+                'city':row[0],
+                'country': row[1],
+                'zipcode': row[2],
+                'coffee_type': row[3],                
+                'quantity': row[4]
+            })
+
+        return jsonify(result)
+
+   # Inside your Flask route or function
+@app.route('/get_order_City')
+def get_order_City():
+    with app.app_context():
+        data = db.session.query(Customers.city, Customers.country, Customers.postal_code, db.func.sum(Order_Items.quantity*Products.size)). \
+        join(Order, Customers.customer_id == Order.customer_id).\
+        join(Order_Items, Order_Items.order_id == Order.order_id).\
+        join(Products, Order_Items.product_id == Products.product_id).group_by(Customers.city, Customers.country,Customers.postal_code).all()
+
+        result = []
+        for row in data:
+            result.append({
+                'city':row[0],
+                'country': row[1],
+                'zipcode': row[2],
+                'quantity': row[3]
             })
 
         return jsonify(result)
